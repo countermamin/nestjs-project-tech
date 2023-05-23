be4ab9c82fd4f3b60729a12adedea09a60bef809
Orzu Maminov <orzu.maminov9@gmail.com>
2023-05-24 00:49:28 +0600

    registeration

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('get-all-users')
-  getUsers() {
-    return this.usersService.getUsers();
+  @Post('create-user')
+  createUsers(@Body() dto: CreateUserDTO) {
+    console.log(dto);
+    return this.usersService.createUser(dto);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { users } from 'src/moks';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './models/user.model';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDTO } from './dto';
+import { AppError } from '../../common/errors';
 
 @Injectable()
 export class UsersService {
-  getUsers() {
-    return users;
+  constructor(
+    @InjectModel(User) private readonly userRepository: typeof User,
+  ) {}
+
+  async hashPassword(password) {
+    return bcrypt.hash(password, 10);
+  }
+
+  async findUserByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email: email } });
+  }
+
+  async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
+    const existUser = await this.findUserByEmail(dto.email);
+    if (existUser) throw new BadRequestException(AppError.USER_EXIST);
+    dto.password = await this.hashPassword(dto.password);
+    await this.userRepository.create({
+      firstName: dto.firstName,
+      username: dto.username,
+      email: dto.email,
+      password: dto.password,
+    });
+    return dto;
   }
 }
