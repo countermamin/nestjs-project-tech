590ea5f68e23a22fbd2f884a50211478f9a0121e
Orzu Maminov <orzu.maminov9@gmail.com>
2023-05-24 21:21:06 +0600

    work with user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Patch, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDTO } from './dto';
+import { UpdateUserDTO } from './dto';
+import { JwtAuthGuard } from '../../guards/jwt-guard';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('create-user')
-  createUsers(@Body() dto: CreateUserDTO) {
-    console.log(dto);
-    return this.usersService.createUser(dto);
+  @ApiTags('API')
+  @ApiResponse({ status: 200, type: UpdateUserDTO })
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+  updateUser(
+    @Body() updateDto: UpdateUserDTO,
+    @Req() request,
+  ): Promise<UpdateUserDTO> {
+    const user = request.user;
+    return this.usersService.updateUser(user.email, updateDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete()
+  deleteUser(@Req() request) {
+    const user = request.user;
+    return this.usersService.deleteUser(user.email);
   }
 }
+
